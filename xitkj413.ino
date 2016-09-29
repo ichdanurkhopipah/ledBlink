f9b0f54d603946d4937cfca0e5dc18141d03f2c5
ichdanurkhopipah <ichda.nur.khopipah13tkj4@gmail.com>
2016-09-29 12:04:13 +0700

    Add files via upload

---
+++ xitkj413.ino
@@ -0,0 +1,20 @@
+Blink
+  Menyalakan LED pada pin 2 selama 4 detik dan Mematikanya selama 4 detik.
+
+   Ini contoh kode Programnya.
+ */
+
+ int led = 2;//Output pada Digital Pin adalah sebuah LED yang terkoneksi/terhubung dengan Digital pin 2.
+  
+void setup() 
+{
+  pinMode(led, OUTPUT);// Mengatur Digital Pin sebagai Output.
+
+}
+
+void loop() {
+  digitalWrite(led, HIGH);          // Mengatur LED agar Menyala.
+  delay(3500);                      // Tunggu sampai 4 detik.
+  digitalWrite(led ,LOW);           // Mengatur LED agar Mati.
+  delay(3500);                      // Tunggu sampai 4 detik. 
+}
